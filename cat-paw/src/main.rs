3ee5e156280955bf2ed6abd5011a46c2c5359980
JoeyShapiro <joeyashapiro@gmail.com>
2024-12-11 19:50:33 -0600

    added usb device

--- cat-paw/src/main.rs
+++ cat-paw/src/main.rs
@@ -37,6 +37,43 @@ use usbd_serial::SerialPort;
 use core::fmt::Write;
 use heapless::String;
 
+/// Custom USB class for our device
+pub struct VendorUSB<'a, B: UsbBus> {
+    interface: InterfaceNumber,
+    in_ep: EndpointIn<'a, B>,
+    out_ep: EndpointOut<'a, B>,
+}
+
+impl<B: UsbBus> VendorUSB<'_, B> {
+    /// Creates a new VendorUSB
+    pub fn new(alloc: &UsbBusAllocator<B>) -> VendorUSB<'_, B> {
+        VendorUSB {
+            interface: alloc.interface(),
+            in_ep: alloc.bulk(64),  // EP1 IN
+            out_ep: alloc.bulk(64), // EP2 OUT
+        }
+    }
+
+    /// Write data to host
+    pub fn write(&mut self, data: &[u8]) -> Result<usize, UsbError> {
+        self.in_ep.write(data)
+    }
+
+    /// Read data from host
+    pub fn read(&mut self, data: &mut [u8]) -> Result<usize, UsbError> {
+        self.out_ep.read(data)
+    }
+}
+
+impl<B: UsbBus> UsbClass<B> for VendorUSB<'_, B> {
+    fn get_configuration_descriptors(&self, writer: &mut DescriptorWriter) -> Result<(), UsbError> {
+        writer.interface(self.interface, 0xFF, 0x00, 0x00)?;
+        writer.endpoint(&self.in_ep)?;
+        writer.endpoint(&self.out_ep)?;
+        Ok(())
+    }
+}
+
 /// Entry point to our bare-metal application.
 ///
 /// The `#[entry]` macro ensures the Cortex-M start-up code calls this function
@@ -79,7 +116,7 @@ fn main() -> ! {
     ));
 
     // Set up the USB Communications Class Device driver
-    let mut serial = SerialPort::new(&usb_bus);
+    let mut serial = VendorUSB::new(&usb_bus);
 
     // Create a USB device with a fake VID and PID
     let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x05ac, 0x1261))
