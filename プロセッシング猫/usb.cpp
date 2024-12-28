0cce372808500844043eeb3610e1486fb2141a10
JoeyShapiro <joeyashapiro@gmail.com>
2024-12-27 19:50:11 -0600

    added final attempt

---
+++ プロセッシング猫/usb.cpp
@@ -0,0 +1,187 @@
+#include <iostream>
+#include <windows.h>
+#include <setupapi.h>
+#include <initguid.h>
+#include <string>
+#include <vector>
+#include <psapi.h>
+#include <chrono>
+#include <thread>
+#include <iomanip>
+
+#include "usb.h"
+
+
+std::vector<USBDeviceInfo> USBDevice::ListDevices() {
+	std::vector<USBDeviceInfo> deviceList;  // Changed from 'devices'
+
+	HDEVINFO hDevInfo = SetupDiGetClassDevs(nullptr, L"USB", nullptr,
+		DIGCF_ALLCLASSES | DIGCF_PRESENT);
+
+	if (hDevInfo == INVALID_HANDLE_VALUE) {
+		return deviceList;
+	}
+
+	SP_DEVINFO_DATA deviceInfoData;
+	deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+
+	// Enumerate through all devices
+	for (DWORD i = 0; SetupDiEnumDeviceInfo(hDevInfo, i, &deviceInfoData); i++) {
+		DWORD regType;
+		CHAR buffer[256];
+		DWORD bufferSize = sizeof(buffer);
+
+		USBDeviceInfo currentDevice;  // Changed variable name
+
+		// Get hardware ID
+		if (SetupDiGetDeviceRegistryProperty(hDevInfo, &deviceInfoData,  // Changed from deviceInfo to hDevInfo
+			SPDRP_HARDWAREID, &regType, (PBYTE)buffer, bufferSize, nullptr)) {
+
+			currentDevice.hardwareId = buffer;
+
+			// Parse VID and PID from hardware ID
+			size_t vidPos = currentDevice.hardwareId.find("VID_");
+			size_t pidPos = currentDevice.hardwareId.find("PID_");
+
+			if (vidPos != std::string::npos && pidPos != std::string::npos) {
+				try {
+					std::string vidStr = currentDevice.hardwareId.substr(vidPos + 4, 4);
+					std::string pidStr = currentDevice.hardwareId.substr(pidPos + 4, 4);
+
+					currentDevice.vendorId = static_cast<USHORT>(std::stoi(vidStr, nullptr, 16));
+					currentDevice.productId = static_cast<USHORT>(std::stoi(pidStr, nullptr, 16));
+				}
+				catch (...) {
+					continue; // Skip if parsing fails
+				}
+			}
+		}
+
+		// Get device description
+		if (SetupDiGetDeviceRegistryProperty(hDevInfo, &deviceInfoData,  // Changed from deviceInfo to hDevInfo
+			SPDRP_DEVICEDESC, &regType, (PBYTE)buffer, bufferSize, nullptr)) {
+
+			// utf-16 to ascii
+			for (size_t i = 0; i < bufferSize; i+=2)
+			{
+				currentDevice.description.append(1, buffer[i]);
+				if (buffer[i] == '\0') break;
+			}
+		}
+
+		deviceList.push_back(currentDevice);
+	}
+
+	SetupDiDestroyDeviceInfoList(hDevInfo);
+	return deviceList;
+}
+
+bool USBDevice::Connect(USHORT vendorId, USHORT productId) {
+	// Get device information set for all USB devices
+	HDEVINFO deviceInfo = SetupDiGetClassDevs(nullptr, L"USB", nullptr,
+		DIGCF_ALLCLASSES | DIGCF_PRESENT);
+
+	if (deviceInfo == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+
+	SP_DEVINFO_DATA deviceInfoData;
+	deviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
+
+	// Enumerate through all devices
+	for (DWORD i = 0; SetupDiEnumDeviceInfo(deviceInfo, i, &deviceInfoData); i++) {
+		DWORD regType;
+		CHAR buffer[256];
+		DWORD bufferSize = sizeof(buffer);
+
+		// Get device path
+		if (SetupDiGetDeviceRegistryProperty(deviceInfo, &deviceInfoData,
+			SPDRP_HARDWAREID, &regType, (PBYTE)buffer, bufferSize, nullptr)) {
+
+			std::string hardwareId = buffer;
+			// Check if this is our device (you'll need to modify this check)
+			if (hardwareId.find("VID_" + std::to_string(vendorId)) != std::string::npos &&
+				hardwareId.find("PID_" + std::to_string(productId)) != std::string::npos) {
+
+				// Get device path for CreateFile
+				SP_DEVICE_INTERFACE_DATA interfaceData;
+				interfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA);
+
+				if (SetupDiCreateDeviceInterface(deviceInfo, &deviceInfoData,
+					nullptr, nullptr, 0, &interfaceData)) {
+
+					// Get required buffer size
+					DWORD requiredSize;
+					SetupDiGetDeviceInterfaceDetail(deviceInfo, &interfaceData,
+						nullptr, 0, &requiredSize, nullptr);
+
+					// Allocate buffer for device interface detail
+					PSP_DEVICE_INTERFACE_DETAIL_DATA detailData =
+						(PSP_DEVICE_INTERFACE_DETAIL_DATA)malloc(requiredSize);
+					detailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
+
+					// Get device interface detail
+					if (SetupDiGetDeviceInterfaceDetail(deviceInfo, &interfaceData,
+						detailData, requiredSize, nullptr, nullptr)) {
+
+						// Open device
+						deviceHandle = CreateFile(detailData->DevicePath,
+							GENERIC_READ | GENERIC_WRITE,
+							FILE_SHARE_READ | FILE_SHARE_WRITE,
+							nullptr,
+							OPEN_EXISTING,
+							FILE_FLAG_OVERLAPPED,
+							nullptr);
+
+						free(detailData);
+						SetupDiDestroyDeviceInfoList(deviceInfo);
+						return deviceHandle != INVALID_HANDLE_VALUE;
+					}
+					free(detailData);
+				}
+			}
+		}
+	}
+
+	SetupDiDestroyDeviceInfoList(deviceInfo);
+	return false;
+}
+
+bool USBDevice::SendData(const void* data, DWORD dataSize) {
+	if (deviceHandle == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+
+	OVERLAPPED overlapped = { 0 };
+	overlapped.hEvent = CreateEvent(nullptr, TRUE, FALSE, nullptr);
+
+	DWORD bytesWritten;
+	bool success = WriteFile(deviceHandle, data, dataSize, &bytesWritten, &overlapped);
+
+	if (!success && GetLastError() == ERROR_IO_PENDING) {
+		// Wait for operation to complete
+		success = GetOverlappedResult(deviceHandle, &overlapped, &bytesWritten, TRUE);
+	}
+
+	CloseHandle(overlapped.hEvent);
+	return success && (bytesWritten == dataSize);
+}
+
+bool USBDevice::ReceiveData(void* buffer, DWORD bufferSize, DWORD* bytesRead) {
+	if (deviceHandle == INVALID_HANDLE_VALUE) {
+		return false;
+	}
+
+	OVERLAPPED overlapped = { 0 };
+	overlapped.hEvent = CreateEvent(nullptr, TRUE, FALSE, nullptr);
+
+	bool success = ReadFile(deviceHandle, buffer, bufferSize, bytesRead, &overlapped);
+
+	if (!success && GetLastError() == ERROR_IO_PENDING) {
+		// Wait for operation to complete
+		success = GetOverlappedResult(deviceHandle, &overlapped, bytesRead, TRUE);
+	}
+
+	CloseHandle(overlapped.hEvent);
+	return success;
+}
---
+++ プロセッシング猫/プロセッシング猫.cpp
@@ -0,0 +1,76 @@
+﻿// プロセッシング猫.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*
+プロ (puro) = "pro"
+セッ (ses) = "ces"
+シン (shin) = "s-in"
+グ (gu) = "g"
+猫 (neko) = "cat"
+*/
+
+#include <iostream>
+#include <pdh.h>
+#include "usb.h"
+
+#pragma comment(lib, "pdh.lib")
+
+/*
+I send over the percentage of usage to the device normalized to its range of 1 byte
+it will convert that to whatever it needs
+in this case it will be used to determine how far it should move from the origin
+*/
+#define u8 unsigned char
+#define u8_max sizeof(u8) * 255
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    printf("max: %d\n", u8_max);
+
+    // Get list of all USB devices
+    auto devices = USBDevice::ListDevices();
+
+    // Print information about each device
+    for (const auto& device : devices) {
+        std::cout << "Description: " << device.description << std::endl;
+        std::cout << "Vendor ID: 0x" << std::hex << device.vendorId << std::endl;
+        std::cout << "Product ID: 0x" << std::hex << device.productId << std::endl;
+        std::cout << "Hardware ID: " << device.hardwareId << std::endl;
+        std::cout << "------------------------" << std::endl;
+    }
+
+    PDH_HQUERY query;
+    PDH_HCOUNTER cpucounter;
+    PDH_HCOUNTER memcounter;
+    PdhOpenQuery(NULL, NULL, &query);
+
+    // this is similar to that mac thing where it asks the script thing for data
+    PdhAddEnglishCounterA(query, "\\Processor Information(_Total)\\% Processor Utility", NULL, &cpucounter);
+    PdhAddEnglishCounterA(query, "\\Memory\\% Committed Bytes in Use", NULL, &memcounter); // "\\Memory\\Available Bytes"
+    PdhCollectQueryData(query);
+
+    PDH_FMT_COUNTERVALUE val;
+
+    while (1) {
+        PdhCollectQueryData(query);
+        PdhGetFormattedCounterValue(cpucounter, PDH_FMT_DOUBLE, NULL, &val);
+        std::cout << "cpu: " << val.doubleValue << "%" << std::endl;
+        
+        MEMORYSTATUSEX memInfo;
+        memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+        GlobalMemoryStatusEx(&memInfo);
+
+        DWORDLONG totalPhysMem = memInfo.ullTotalPhys;
+        DWORDLONG physMemUsed = totalPhysMem - memInfo.ullAvailPhys;
+
+        float mem = (float)physMemUsed / totalPhysMem;
+
+        // normalize to byte range
+        u8 norm = mem * u8_max;
+        printf("mem: %d\n", norm);
+
+        Sleep(1000);
+    }
+}
